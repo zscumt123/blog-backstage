7316f5e2372bd51eaad7015c5b2824cf7ab98305
zs <zscumt123@163.com>
2018-01-17 20:56:30 +0800

    category

--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import CommonTable from '../../components/commonTable/CommonTable';
 
-export default class Category extends Component {
+const columns = [
+    {
+        title: '分类名称',
+        dataIndex: 'category_name',
+        key: 'category_name'
+    },
+    {
+        title: '创建时间',
+        dataIndex: 'create_time',
+        key: 'create_time'
+    }
+]
 
+class Category extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
     render() {
         return (
-            
+            <div>
+                <CommonTable/>
+            </div>
         );
     }
 }
-
+const mapStateToProps = (state) => ({
+    ...state.categoryData.data,
+    loading: state.categoryData.loading
+})
+export default connect(mapStateToProps)(Category)
